6c9d2912b5d6492308405addec3bee226aa7ec94
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2022-11-21 10:00:37 -0300

    modificações

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,7 +9,10 @@ import { defaultValues, IFormLogin } from "./types";
 
 const schema = yup
   .object({
-    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    email: yup
+    .string()
+    .email("E-mail inválido")
+    .required("Campo obrigatório"),
     password: yup
       .string()
       .min(6, "No minimo 6 caracteres")
@@ -27,6 +30,8 @@ const Login = () => {
     defaultValues,
     reValidateMode: "onChange",
   });
+
+  console.log(isValid)
 
   return (
     <Container>
@@ -49,7 +54,7 @@ const Login = () => {
             errorMessage={errors?.password?.message}
           />
           <Spacing />
-          <Button title="Entrar" />
+          <Button title="Entrar"/>
         </Column>
       </LoginContainer>
     </Container>
